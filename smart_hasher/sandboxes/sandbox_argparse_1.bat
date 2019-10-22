e1ea8f0de75f441c0d11ac463c70988f302bbec0
sergtk <@>
2019-10-23 02:59:32 +0300

    refs #10: * rewrite parsing command line parameters with module 'argparse' * partial adjustments of naming conventions

---
+++ smart_hasher/sandboxes/sandbox_argparse_1.bat
@@ -0,0 +1,3 @@
+:: python.exe sandbox_argparse_1.py --input_file file1 --input_file=file2 -if file3 -if=file4
+python.exe sandbox_argparse_1.py -h
+
---
+++ smart_hasher/sandboxes/sandbox_argparse_2.bat
@@ -0,0 +1,2 @@
+:: python.exe sandbox_argparse_2.py 4 2 -v --quiet
+python.exe sandbox_argparse_2.py -h
