33c89abfe8426311493798cd81a7c78d3ba85766
sergtk <@>
2019-10-23 03:19:13 +0300

    refs #10: adjustments of naming conventions for variables and functions

--- smart_hasher/smart_hasher.py
+++ smart_hasher/smart_hasher.py
@@ -6,94 +6,85 @@ from datetime import datetime
 import math
 import argparse
 
-fileChunkSize = 1024 * 1024
-# fileChunkSize = 500
+file_chunk_size = 1024 * 1024
+# file_chunk_size = 500
 
-sizeNames = ("B", "Kb", "Mb", "Gb", "Tb", "Pb", "Eb", "Zb", "Yb")
+size_names = ("B", "Kb", "Mb", "Gb", "Tb", "Pb", "Eb", "Zb", "Yb")
 
 # Ref: "Better way to convert file sizes in Python" https://stackoverflow.com/a/14822210/13441
-def convertSize(sizeBytes: float) -> str:
-   global sizeNames
+def convert_size(size_bytes: float) -> str:
+   global size_names
 
-   if sizeBytes == 0:
-       return "0 " + sizeNames[0]
-   # sizeNames = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
-   i = int(math.floor(math.log(sizeBytes, 1024)))
+   if size_bytes == 0:
+       return "0 " + size_names[0]
+   # size_names = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+   i = int(math.floor(math.log(size_bytes, 1024)))
    p = math.pow(1024, i)
-   s = round(sizeBytes / p, 2)
-   return "%s %s" % (s, sizeNames[i])
+   s = round(size_bytes / p, 2)
+   return "%s %s" % (s, size_names[i])
 
-def formatSeconds(seconds: float) -> str:
+def format_seconds(seconds: float) -> str:
     # seconds = int(diff.total_seconds());
     sec = int(seconds)
     ret =  "{0}:{1:02d}:{2:02d}".format(int(sec / 60 / 60), int(sec / 60) % 60, sec % 60)
     return ret
 
-# This function is not used 
-def calcHash(fileName):
-    # Ref: https://www.guru99.com/reading-and-writing-files-in-python.html
-    f = open(fileName, "rb")
-    hasher = hashlib.md5()
-    hasher.update(f.read())
-    hash = hasher.hexdigest()
-    return hash
-
 # Ref: https://stackoverflow.com/questions/9181859/getting-percentage-complete-of-an-md5-checksum
-def calcHashWithProgress(fileName):
-    curSize = 0
-    prevPercent = 0
+def calc_hash(file_name):
+    cur_size = 0
+    prev_percent = 0
     hasher = hashlib.md5()
-    totalSize = os.path.getsize(fileName)
+    total_size = os.path.getsize(file_name)
 
-    recentMoment = startMoment = datetime.now();
-    recentSize = 0
-    recentSpeed = 0
-    recentSpeedReadable = "-"
+    recent_moment = start_moment = datetime.now();
+    recent_size = 0
+    recent_speed = 0
+    recent_speed_readable = "-"
 
     data = True
-    f = open(fileName, "rb")
+    f = open(file_name, "rb")
 
     while data:
         # Read and update digest.
-        data = f.read(fileChunkSize)
-        curSize += len(data)
+        data = f.read(file_chunk_size)
+        cur_size += len(data)
         hasher.update(data)
 
-        recentSize += len(data)
+        recent_size += len(data)
 
         # Calculate progress.
-        percent = int(10000 * curSize / totalSize)
+        percent = int(10000 * cur_size / total_size)
 
         # Ref: https://www.pythoncentral.io/pythons-time-sleep-pause-wait-sleep-stop-your-code/
         # time.sleep(1)
 
-        if percent > prevPercent:
-            curMoment = datetime.now();
-            elapsedSeconds = (curMoment - startMoment).total_seconds()
-            remainSeconds = elapsedSeconds / percent * (10000 - percent);
+        if percent > prev_percent:
+            cur_moment = datetime.now();
+            elapsed_seconds = (cur_moment - start_moment).total_seconds()
+            remain_seconds = elapsed_seconds / percent * (10000 - percent);
 
-            speed = curSize / elapsedSeconds;
-            speedReadable = convertSize(speed);
+            speed = cur_size / elapsed_seconds;
+            speed_readable = convert_size(speed);
 
-            recentSeconds = (curMoment - recentMoment).total_seconds()
-            if (recentSeconds > 3 and recentSize > 0):
-                recentSpeed = recentSize / recentSeconds
-                recentSpeedReadable = convertSize(recentSpeed)
-                recentMoment = curMoment
-                recentSize = 0
+            recent_seconds = (cur_moment - recent_moment).total_seconds()
+            if (recent_seconds > 3 and recent_size > 0):
+                recent_speed = recent_size / recent_seconds
+                recent_speed_readable = convert_size(recent_speed)
+                recent_moment = cur_moment
+                recent_size = 0
 
             # Ref: "Using multiple arguments for string formatting in Python (e.g., '%s … %s')" https://stackoverflow.com/a/3395158/13441
             # Ref: "Display number with leading zeros" https://stackoverflow.com/a/134951/13441
             print ('{0}.{1:02d}% done ({2:,d} bytes). Remaining time: {3}. File average speed: {4}/sec. Recent speed: {5}/sec.   \r'.
-                   format(int(percent / 100), int(percent % 100), curSize, formatSeconds(remainSeconds), speedReadable, recentSpeedReadable),
+                   format(int(percent / 100), int(percent % 100), cur_size, format_seconds(remain_seconds), speed_readable, recent_speed_readable),
                    end="")
-            prevPercent = percent
+            prev_percent = percent
     f.close()
     print ('                                                                                      \r', end="") # Clear line
     return hasher.hexdigest()
 
-def get_output_file_name(inputFileName):
-    output_file_name = inputFileName + ".md5"
+def get_output_file_name(input_file_name):
+    output_file_name = input_file_name + ".md5"
 
     if cmd_line_args.hash_file_name_output_postfix:
         output_file_name += "." + cmd_line_args.hash_file_name_output_postfix
@@ -110,37 +101,36 @@ def parse_command_line():
 
     cmd_line_args = parser.parse_args()
 
-def getDateTimeStr(dateTime: datetime) -> str:
+def get_date_time_str(dateTime: datetime) -> str:
     return dateTime.strftime("%Y.%m.%d %H:%M:%S")
 
 # Returns false if hash not calculated, probably because it was already calculated.
-def handle_input_file(inputFileName):
-    startDateTime = datetime.now();
-    print("Handle file start time: " + getDateTimeStr(startDateTime) + " (" + inputFileName + ")")
-    outputFileName = get_output_file_name(inputFileName)
+def handle_input_file(input_file_name):
+    start_date_time = datetime.now();
+    print("Handle file start time: " + get_date_time_str(start_date_time) + " (" + input_file_name + ")")
+    output_file_name = get_output_file_name(input_file_name)
     # Ref: https://stackoverflow.com/questions/82831/how-do-i-check-whether-a-file-exists-without-exceptions
-    if (os.path.exists(outputFileName)):
-        print("Output file name '" + outputFileName + "' exists ... calculation of hash skipped.")
+    if (os.path.exists(output_file_name)):
+        print("Output file name '" + output_file_name + "' exists ... calculation of hash skipped.")
         return False
-    print("Calculate hash for file '" + inputFileName + "'...")
-    # hash = calcHash(inputFileName)
-    hash = calcHashWithProgress(inputFileName)
+    print("Calculate hash for file '" + input_file_name + "'...")
+    hash = calc_hash(input_file_name)
 
     # Ref: https://stackoverflow.com/questions/6159900/correct-way-to-write-line-to-file
-    with open(outputFileName, 'a') as outputFile:
+    with open(output_file_name, 'a') as outputFile:
         # outputFile.write("# MD5 hash generated by smart_hasher\n\n")
-        outputFile.write(hash + " *" + inputFileName + "\n")
+        outputFile.write(hash + " *" + input_file_name + "\n")
 
-    print("HASH: ", hash, "(" + outputFileName + ")")
+    print("HASH: ", hash, "(" + output_file_name + ")")
 
-    endDateTime = datetime.now();
-    print("Handle file end time: " + getDateTimeStr(endDateTime) + " (" + inputFileName + ")")
-    seconds = int((endDateTime - startDateTime).total_seconds());
+    end_date_time = datetime.now();
+    print("Handle file end time: " + get_date_time_str(end_date_time) + " (" + input_file_name + ")")
+    seconds = int((end_date_time - start_date_time).total_seconds());
     # print("Elapsed time: {0}:{1:02d}:{2:02d}".format(int(seconds / 60 / 60), int(seconds / 60) % 60, seconds % 60))
 
-    fileSize = os.path.getsize(inputFileName)
-    speed = fileSize / seconds if seconds > 0 else 0
-    print("Elapsed time: {0} (Average speed: {1}/sec)".format(formatSeconds(seconds), convertSize(speed)))
+    file_size = os.path.getsize(input_file_name)
+    speed = file_size / seconds if seconds > 0 else 0
+    print("Elapsed time: {0} (Average speed: {1}/sec)".format(format_seconds(seconds), convert_size(speed)))
       
     return True
 try:
